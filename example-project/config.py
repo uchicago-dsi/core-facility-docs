52a4b6e01ef038f7f2512955ca6c251e4689f82c
toddnief <todd.nief@gmail.com>
2023-10-17 14:16:17 -0500

    add example-project files

---
+++ example-project/config.py
@@ -0,0 +1,10 @@
+"""Defines constants used across the application.
+"""
+
+import os
+
+# define constants for directory paths
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = ROOT_DIR + "/data"
+NOTEBOOKS_DIR = ROOT_DIR + "/notebooks"
+UTILS_DIR = ROOT_DIR + "/utils"
